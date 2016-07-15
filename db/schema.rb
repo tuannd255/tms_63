8d75329707c1049146da2af3ae1f351471ce2d1c
tuannd255 <tuannd255.com>
2016-07-15 15:58:06 +0700

    finish manage user 2

--- db/schema.rb
+++ db/schema.rb
@@ -92,15 +92,15 @@ ActiveRecord::Schema.define(version: 20160714011543) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.boolean  "supervisor",             default: false
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.integer  "role",                   default: 0
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
