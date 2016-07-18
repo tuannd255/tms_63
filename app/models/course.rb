b68dc175db09475abab5e7db702385e231ce64ad
toxingau <toxingau@gmail.com>
2016-07-18 23:05:46 +0700

    finish task#60943/start-finish-course

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,4 +5,9 @@ class Course < ActiveRecord::Base
   has_many :subjects, through: :course_subjects
 
   validates :subjects, presence: true
+  validates :description, presence: true
+
+  accepts_nested_attributes_for :subjects, allow_destroy: true
+
+  enum status: {init: 0, start: 1, finish: 2}
 end
