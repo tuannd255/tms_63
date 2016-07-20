ca735bc47db8239194b0b0516db0aaf54cee13fd
toxingau <toxingau@gmail.com>
2016-07-20 10:00:34 +0700

    finsih task supervisor start/finish subject

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
-User.create!(name: "Pham Van Teo",
-  email: "test@gmail.com",
-  password: "123456",
-  password_confirmation: "123456",
-  role: 2)
+User.create! name: "Pham Van Teo", email: "test@gmail.com",
+  password: "1", password_confirmation: "1", role: 2
+  
+User.create! name: "Pham Van Ty", email: "1@gmail.com",
+  password: "1", password_confirmation: "1", role: 1
+
+User.create! name: "Pham Van Tit", email: "2@gmail.com",
+  password: "2", password_confirmation: "2", role: 0
